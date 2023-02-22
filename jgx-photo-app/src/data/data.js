319f56f9cabb53c3f3391c4ec427314903840076
Jose G Martinez <jgermanmm@gmail.com>
2023-02-22 01:47:39 -0300

    Rutas relativas para las img de data y nav

--- jgx-photo-app/src/data/data.js
+++ jgx-photo-app/src/data/data.js
@@ -4,7 +4,7 @@ export const projects = [
       subtitle: "Bodas, eventos",
       description:
         "Nos apasiona capturar los momentos más importantes de la vida, desde bodas y aniversarios hasta los primeros pasos de un bebé.",
-      image: "/public/IMG_9609.jpg",
+      image: "./public/IMG_9609.jpg",
       link: "#",
     },
     {
@@ -12,7 +12,7 @@ export const projects = [
       subtitle: "Retrato, familiar",
       description:
         "Cada una de nuestras imágenes cuenta una historia y estamos comprometidos en capturar los momentos más significativos en la vida de cada uno de nuestros clientes.",
-      image: "/public/IMG_9929.jpg",
+      image: "./public/IMG_9929.jpg",
       link: "#",
     },
     {
@@ -20,7 +20,7 @@ export const projects = [
       subtitle: "Gastronomía, restaurantes",
       description:
         "Nos enorgullece ofrecer una amplia gama de servicios personalizados para cada uno de nuestros clientes, y estamos seguros de que encontrará justo lo que necesita para satisfacer sus necesidades fotográficas.",
-      image: "/public/IMG_9406.jpg",
+      image: "./public/IMG_9406.jpg",
       link: "#",
     },
     {
@@ -28,7 +28,7 @@ export const projects = [
       subtitle: "Producto, ventas, catálogos",
       description:
         "Nos enorgullece ofrecer una amplia gama de servicios personalizados para cada uno de nuestros clientes, y estamos seguros de que encontrará justo lo que necesita para satisfacer sus necesidades fotográficas.",
-      image: "/public/IMG_8256.jpg",
+      image: "./public/IMG_8256.jpg",
       link: "#",
     }
   ];
