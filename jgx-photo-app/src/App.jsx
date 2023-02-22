98a508c9db11889a16aa3a431a4735a23d0e4d96
Jose G Martinez <jgermanmm@gmail.com>
2023-02-22 01:41:05 -0300

    Segundo commit, estructura lista. about-projects

--- jgx-photo-app/src/App.jsx
+++ jgx-photo-app/src/App.jsx
@@ -1,33 +1,24 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import NavBar from './components/NavBar/NavBar'
+import About from './components/About/About'
+import Projects from './components/Projects/Projects'
+import Skills from './components/Skills/Skills'
+import Testimonials from './components/Testimonials/Testimonials'
+import Contact from './components/Contact/Contact'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <main className="text-gray-400 bg-black body-font">
+    <NavBar />
+    <About />
+    <Projects />
+    <Skills />
+    <Testimonials />
+    <Contact />
+  </main>
   )
 }
 
---
+++ jgx-photo-app/src/data/data.js
@@ -0,0 +1,34 @@
+export const projects = [
+    {
+      title: "Fotografía de bodas",
+      subtitle: "Bodas, eventos",
+      description:
+        "Nos apasiona capturar los momentos más importantes de la vida, desde bodas y aniversarios hasta los primeros pasos de un bebé.",
+      image: "/public/IMG_9609.jpg",
+      link: "#",
+    },
+    {
+      title: "Fotografía de retrato",
+      subtitle: "Retrato, familiar",
+      description:
+        "Cada una de nuestras imágenes cuenta una historia y estamos comprometidos en capturar los momentos más significativos en la vida de cada uno de nuestros clientes.",
+      image: "/public/IMG_9929.jpg",
+      link: "#",
+    },
+    {
+      title: "Fotografía gastronómica",
+      subtitle: "Gastronomía, restaurantes",
+      description:
+        "Nos enorgullece ofrecer una amplia gama de servicios personalizados para cada uno de nuestros clientes, y estamos seguros de que encontrará justo lo que necesita para satisfacer sus necesidades fotográficas.",
+      image: "/public/IMG_9406.jpg",
+      link: "#",
+    },
+    {
+      title: "Fotografía de producto",
+      subtitle: "Producto, ventas, catálogos",
+      description:
+        "Nos enorgullece ofrecer una amplia gama de servicios personalizados para cada uno de nuestros clientes, y estamos seguros de que encontrará justo lo que necesita para satisfacer sus necesidades fotográficas.",
+      image: "/public/IMG_8256.jpg",
+      link: "#",
+    }
+  ];
