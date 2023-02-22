86f1a1f6402c35c8da3df267aa6495fbc856dda5
Jose G Martinez <jgermanmm@gmail.com>
2023-02-22 10:21:54 -0300

    Importar img front y renderizar

--- jgx-photo-app/src/components/About/About.jsx
+++ jgx-photo-app/src/components/About/About.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import imgFront from '../../assets/img/front_img.jpg';
 
 const About = () => {
   return (
@@ -29,7 +30,7 @@ const About = () => {
           <img
             className="object-cover object-center rounded"
             alt="hero"
-            src="src/assets/img/front_img.jpg"
+            src={imgFront}
           />
         </div>
       </div>
