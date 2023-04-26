033664c7becd638e62e0e339e95e1018331ff361
Jose Germán Martínez <jgermanmm@gmail.com>
2023-04-26 16:26:39 -0400

    Se agrega formulario de contacto y maps

--- jgx-photo-app/src/App.jsx
+++ jgx-photo-app/src/App.jsx
@@ -1,25 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import NavBar from './components/NavBar/NavBar'
-import About from './components/About/About'
-import Projects from './components/Projects/Projects'
-import Skills from './components/Skills/Skills'
-import Testimonials from './components/Testimonials/Testimonials'
-import Contact from './components/Contact/Contact'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
+import Skills from "./components/Skills/Skills";
+import Testimonials from "./components/Testimonials/Testimonials";
+import Contact from "./components/Contact/Contact";
 
 function App() {
-
   return (
     <main className="text-gray-400 bg-black body-font p-0">
-    <NavBar />
-    <About />
-    <Projects />
-    <Skills />
-    <Testimonials />
-    <Contact />
-  </main>
-  )
+      <NavBar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
 }
 
-export default App
+export default App;
