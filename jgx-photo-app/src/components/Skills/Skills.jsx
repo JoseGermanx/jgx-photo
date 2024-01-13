1b0d5ff5785eb47b0e5480d9f40f93be95b98c9e
Jose Germán Martínez <jgermanmm@gmail.com>
2024-01-12 21:51:18 -0300

    textos servicios

--- jgx-photo-app/src/components/Skills/Skills.jsx
+++ jgx-photo-app/src/components/Skills/Skills.jsx
@@ -12,9 +12,7 @@ const Skills = () => {
             Servicios que ofrecemos
           </h1>
           <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi sit
-            ipsa delectus eum quo voluptas aspernatur accusantium distinctio
-            possimus est.
+            Cuentas con el respaldos de mas de 15 años de experiencia, entregando soluciones de calidad a nuestros clientes.
           </p>
         </div>
         <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
