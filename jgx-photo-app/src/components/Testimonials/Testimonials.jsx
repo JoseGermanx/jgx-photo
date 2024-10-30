476a632ecb456207416a39626ae2a4d8a52b7564
Jose Germán Martínez <jgermanmm@gmail.com>
2024-10-30 12:15:42 -0300

    Refactorizar componente Testimonials
    Se refactoriza el componente Testimonials para cambiar el color de fondo a blanco y actualizar el título a "Clientes". Además, se agregan dos nuevos clientes con sus respectivos logos.

--- jgx-photo-app/src/components/Testimonials/Testimonials.jsx
+++ jgx-photo-app/src/components/Testimonials/Testimonials.jsx
@@ -4,15 +4,19 @@ import { LinkIcon } from "@heroicons/react/solid";
 const Testimonials = () => {
   return (
     <section id="clients">
-    <div className="container px-5 py-10 mx-auto bg-slate-400">
+    <div className="container px-5 py-10 mx-auto bg-white">
       <div className="text-center mb-20">
         <LinkIcon className="w-10 inline-block mb-4" />
-        <h2 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
-          Marcas
+        <h2 className="sm:text-6xl text-3xl font-medium title-font text-yellow-700 mb-4">
+          Clientes
         </h2>
-        <a href='https://danpaintinggroup.com/' target='_blank'>
-        <img src="https://res.cloudinary.com/dh6ipg8bj/image/upload/v1727121067/logo_dan_painting_group_bg_289_o7fbsn.png" alt="Client DAN Painting Group" className="inline-block mt-4 grayscale animate-pulse" width={200}/></a>
-      </div>
+        <a className="mx-7" href='https://danpaintinggroup.com/' target='_blank'>
+        <img src="https://res.cloudinary.com/dh6ipg8bj/image/upload/v1727121067/logo_dan_painting_group_bg_289_o7fbsn.png" alt="Client DAN Painting Group - Orlando" className="inline-block mt-4 animate-pulse" width={200}/></a>
+       <a href='https://www.tanniesfoods.com/' target='_blank'>
+        <img src="https://res.cloudinary.com/dh6ipg8bj/image/upload/v1730300686/Tannies-06_rfsryn.avif" alt="Client Tannies Food - Orlando" className="inline-block mt-4 animate-pulse" width={200}/></a>
+        </div>
+     
+      
     </div>
   </section>
   )
