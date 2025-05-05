4b7c05841399b8bfd4347f3166c35841f5cd4be8
José G. Martínez <jgermanmm@gmail.com>
2025-05-05 17:45:12 -0400

    Agregar uso de usePageSEO en el componente ServicesView para mejorar la optimización SEO

--- jgx-photo-app/src/views/ServicesView.jsx
+++ jgx-photo-app/src/views/ServicesView.jsx
@@ -2,8 +2,21 @@
 
 import React from 'react'
 import Skills from '../components/Skills/Skills'
+import usePageSEO from '../hooks/usePageSEO';
 
 export default function ServicesView() {
+  usePageSEO({
+    title: "Servicios - Marketing Digital y SEO",
+    description:
+      "Somos una agencia digital especializada en marketing digital y SEO. Nuestro equipo de expertos en marketing digital y SEO te ayudará a alcanzar tus objetivos en internet. Contáctanos para obtener más información.",
+    keywords: ["Marketing", "SEO"],
+    ogTitle: "Servicios - Marketing Digital y SEO",
+    ogDescription: "Somos una agencia digital especializada en marketing digital y SEO. Nuestro equipo de expertos en marketing digital y SEO te ayudará a alcanzar tus objetivos en internet. Contáctanos para obtener más información.",
+    ogImage: "https://res.cloudinary.com/dh6ipg8bj/image/upload/v1727211451/Purple_Black_Modern_Marketing_Plan_Presentation_eabtxb.png",
+    ogUrl: "https://www.jgx.agency/",
+  });
+
+  
   return (
     <>
     <Skills />
