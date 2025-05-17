83b258b3658b23e0b7a614622b0fec02f8b7eaca
José G. Martínez <jgermanmm@gmail.com>
2025-05-16 21:28:33 -0400

    chat boot

--- jgx-photo-app/src/views/Home.jsx
+++ jgx-photo-app/src/views/Home.jsx
@@ -4,8 +4,13 @@ import Skills from "../components/Skills/Skills";
 import Testimonials from "../components/Testimonials/Testimonials";
 import Contact from "../components/Contact/Contact";
 import usePageSEO from "../hooks/usePageSEO";
+import { useEffect } from 'react';
+import '@n8n/chat/style.css';
+import { createChat } from '@n8n/chat';
+
 
 export default function Home() {
+
   usePageSEO({
     title: "Marketing Digital y SEO - JGx Agency",
     description:
@@ -16,6 +21,38 @@ export default function Home() {
     ogImage: "https://res.cloudinary.com/dh6ipg8bj/image/upload/v1727211451/Purple_Black_Modern_Marketing_Plan_Presentation_eabtxb.png",
     ogUrl: "https://www.jgx.agency/",
   });
+
+  useEffect(() => {
+		createChat({
+			webhookUrl: 'https://primary-production-8825.up.railway.app/webhook/0498abd7-9cec-4463-af42-5c0dd8e8beaf/chat',
+      webhookConfig: {
+		method: 'POST',
+		headers: {}
+	},
+	target: '#n8n-chat',
+	mode: 'window',
+	chatInputKey: 'chatInput',
+	chatSessionKey: 'sessionId',
+	metadata: {},
+	showWelcomeScreen: false,
+	defaultLanguage: 'en',
+	initialMessages: [
+		'Hola! 👋',
+		'Soy el chat de betacode. ¿Cómo te puedo ayudar hoy?'
+	],
+	i18n: {
+		en: {
+			title: 'SEO y Marketing Digital',
+			subtitle: "Inicia el chat. Te responderé preguntas sobre nuestros servicios.",
+			footer: '',
+			getStarted: 'Nueva conversación',
+			inputPlaceholder: 'Escribe tu consulta...',
+		},
+	},
+		});
+	}, []);
+ 
+
   return (
     <>
       <About />
